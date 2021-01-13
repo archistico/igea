43be34ef56050a9af07c5fbe9e62469bdf825220
archistico <info@archistico.com>
2021-01-13 22:24:55 +0100

    add filesize

--- listafile.php
+++ listafile.php
@@ -1,14 +1,28 @@
 <?php
-function getDirContents($dir, &$results = array()) {
+
+function checkExtension($ext)
+{
+    return in_array($ext, ['jpg', 'png']);
+}
+
+function getDirContents($dir, &$results = array())
+{
     $files = scandir($dir);
 
     foreach ($files as $key => $value) {
         $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
         if (!is_dir($path)) {
-            $results[] = [
-                'path' => $path,
-                'md5'  => md5_file($path)
-        ];
+            $path_parts = pathinfo($path);
+            if (isset($path_parts['extension']) && checkExtension($path_parts['extension'])) {
+                $results[] = [
+                    'dirname' => $path_parts['dirname'],
+                    'basename' => $path_parts['basename'],
+                    'filename' => $path_parts['filename'],
+                    'extension' => $path_parts['extension'],
+                    'md5'  => md5_file($path),
+                    'size' => filesize($path)
+                ];
+            }
         } else if ($value != "." && $value != "..") {
             getDirContents($path, $results);
             // SEGNO LE DIRECTORY $results[] = $path;
@@ -18,4 +32,14 @@ function getDirContents($dir, &$results = array()) {
     return $results;
 }
 
-var_dump(getDirContents('/home/archemi/igea'));
+$immagini = getDirContents('/home/archemi/igea');
+$contatore = 0; 
+foreach($immagini as $i) {
+    $contatore++;
+    echo $contatore." FILE: ".$i['dirname']."".$i['basename']." MD5: ".$i['md5']." SIZE: ".$i['size']. "\n";
+}
+
+/*
+
+
+*/
